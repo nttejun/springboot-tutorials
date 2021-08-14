2d4d7ac614570fc7b6a3202da6aaca25d00113e4
nttejun <nttejun@gmail.com>
2021-08-14 10:11:47 +0900

    Proxy bean, Spring Aop 차이 확인

--- aop/src/main/java/com/tutorial/aop/AopApplication.java
+++ aop/src/main/java/com/tutorial/aop/AopApplication.java
@@ -1,13 +1,16 @@
 package com.tutorial.aop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AopApplication.class, args);
+		SpringApplication app = new SpringApplication(AopApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
 	}
 
 }
