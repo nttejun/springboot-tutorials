78618d69add12ac5ff8602534f2ee9bc8cbefd4b
nttejun <nttejun@gmail.com>
2021-12-18 18:16:09 +0900

    Team 객체 column, toString 추가

--- querydsl/src/main/java/com/tutorials/querydsl/entity/Team.java
+++ querydsl/src/main/java/com/tutorials/querydsl/entity/Team.java
@@ -2,6 +2,7 @@ package com.tutorials.querydsl.entity;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,13 +10,16 @@ import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
 public class Team {
 
   @Id @GeneratedValue
+  @Column (name = "team_id")
   private Long id;
   private String name;
 
